f8d6600dd41af667d74aebf62ce8f542cda76891
cfowler650 <cfowler650@gmail.com>
2019-12-11 09:57:06 -0600

    Create Components & Screens Folder, AddTodo, TodoList, and Home Screens. Add react-navigation / react-native-gesture-handler.

---
+++ src/screens/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Todo List</Text>
+      </View>
+    );
+  }
+}
